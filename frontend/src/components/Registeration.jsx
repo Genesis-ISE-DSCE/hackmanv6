360644d5aa5e38303307299b9ee5dd3792a93fa8
Astraxx04 <gagan200254@gmail.com>
2023-04-07 18:58:39 +0530

    Images

--- frontend/src/components/Registeration.jsx
+++ frontend/src/components/Registeration.jsx
@@ -1,6 +1,10 @@
 function Registeration() {
     return (
-        <div>Registeration</div>
+        <div className="bg-[#24263B]">
+            <div className="text-5xl text-[#22C3FF] text-center">REGISTER</div>
+        
+        
+        </div>
     );
 }
 
