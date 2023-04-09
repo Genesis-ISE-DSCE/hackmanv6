43539d7b7b08d50ad5317a0bd160e606f716795b
Janesh7 <janeshwalia273@gmail.com>
2023-04-09 17:02:06 +0530

    Modules registration

--- backend/modules/Registeration.js
+++ backend/modules/Registeration.js
@@ -1,1 +1,40 @@
 // define the schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RegistrationSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  college: {
+    type: String,
+    required: true
+  },
+  teamName: {
+    type: String,
+    required: true
+  },
+  teamMembers: {
+    type: [String],
+    maxlength: 4,
+    required: true
+  },
+  theme: {
+    type: String,
+    required: true
+  }
+});
+
+const Registration = mongoose.model('Registration', RegistrationSchema);
+
+module.exports = Registration;
