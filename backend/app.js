b522cab8c443043081ec25f80c3767d1d688a674
heisdinesh <dinesh35382@gmail.com>
2023-04-09 10:32:23 +0530

    backend setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectWithDb = require("./config/db");
+const cookieParser = require("cookie-parser")
 
 // connect with database
 connectWithDb();
@@ -9,7 +10,9 @@ const app = express();
 
 // regular middlewares
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
 
 
 
