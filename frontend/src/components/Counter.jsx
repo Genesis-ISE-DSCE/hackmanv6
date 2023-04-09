2004d930d9bee0ba4c3bf65672f5d4942998d223
Astraxx04 <gagan200254@gmail.com>
2023-04-09 13:26:35 +0530

    Counter Design updates

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -20,38 +20,38 @@ function Counter(props) {
     const seconds = Math.floor(countdown % 60).toLocaleString('en-US',{minimumIntegerDigits:2,useGrouping:false});
 
     return(
-        <div id="landing" className="bg-[#27273F] lg:pb-12 md:pb-8 pb-16 pt-36">
+        <div id="landing" className="bg-[#27273F] lg:pb-36 lg:pt-8 md:pb-8 md:pt-8 pb-8">
             <section>
-                <div className="items-center justify-center">
-                    <div className="items-center justify-center" style={{ position: "relative", top: "-30px" }}>
-                        <img className="sm:px-[20%] px-[10%] pt-28 pb-16" alt="Poster1" src={require('../assets/images/hackman_logo.png')} />
+                <div className="lg:items-center lg:justify-center md:items-center md:justify-center items-center justify-center">
+                    <div className="lg:items-center lg:justify-center md:items-center md:justify-center items-center justify-center" style={{ position: "relative", top: "-30px" }}>
+                        <img className="lg:pt-24 lg:pb-8 lg:w-auto md:pt-24 md:pb-8 md:w-auto pt-16 w-auto px-8 mx-auto" alt="Poster1" src={require('../assets/images/hackman_logo.png')} />
                     </div>
                     <div>
-                        <img className="lg:float-left lg:ml-48 md:float-left md:ml-8 float-left" src={eva_bg_small_lef} alt="Registration eva" />
-                        <img className="lg:float-right lg:mr-48 md:float-right md:mr-8 float-right" src={eva_bg_small_rig} alt="Registration eva" />
+                        <img className="lg:float-left lg:ml-48 lg:w-[15%] md:float-left md:ml-8 md:w-[10%] float-left w-24" src={eva_bg_small_lef} alt="Registration eva" />
+                        <img className="lg:float-right lg:mr-48 lg:w-[15%] md:float-right md:mr-8 md:w-[10%] float-right w-24" src={eva_bg_small_rig} alt="Registration eva" />
                     </div>
-                    <h1 className="font-bold mb-8 text-center justify-center sm:text-5xl text-3xl mb-0 lg:pt-20 md:pt-14 pt-8" style={{ color: '#D4DFC7' }}>JUNE 4th-5th</h1>
-                    <h2 className="font-bold mb-8 text-center justify-center sm:text-3xl text-1xl mb-0" style={{ color: '#22C3FF' }}>&lt;BreakIt&gt;  &lt;MakeIt&gt;  &lt;OwnIt&gt;</h2>
+                    <h1 className="lg:pt-20 lg:text-5xl md:pt-14 md:text-3xl font-bold text-center justify-center text-2xl pt-8 pb-4 text-[#D4DFC7]">JUNE 4th-5th</h1>
+                    <h2 className="lg:text-3xl md:text-2xl font-bold mb-8 text-center justify-center text-1xl text-[#22C3FF]">&lt;BreakIt&gt;  &lt;MakeIt&gt;  &lt;OwnIt&gt;</h2>
                 </div>
             </section>
-        <div className="flex flex-wrap justify-center">
-            <div className="flex flex-col items-center mr-6 rounded-lg p-2 bg-gray-200 sm:w-1/8" style={{ background: "#23354E" }}>
-                <div className="text-4xl sm:text-12xl font-bold text-7xl p-8" style={{ color: '#FFFF' }}>{days}</div>
-                <div className="text-2xl font-bold text-2xl" style={{ color: '#FFFF' }}>Days</div>
+            <div className="flex flex-wrap justify-center">
+                <div className="lg:flex lg:flex-col lg:items-center lg:mr-6 lg:rounded-lg lg:p-4 lg:h-auto lg:w-44 md:flex md:flex-col md:items-center md:mr-4 md:rounded-lg md:p-4 md:h-auto md:w-36 flex flex-col items-center w-16 mr-4 rounded-lg p-2 h-16 bg-[#23354E]">
+                    <div className="lg:text-7xl lg:p-6 md:text-7xl md:p-4 text-3xl font-bold text-[#FFFFFF]">{days}</div>
+                    <div className="lg:text-2xl md:text-2xl text-sm font-bold text-[#FFFFFF]">Days</div>
+                </div>
+                <div className="lg:flex lg:flex-col lg:items-center lg:mr-6 lg:rounded-lg lg:p-4 lg:h-auto lg:w-44 md:flex md:flex-col md:items-center md:mr-4 md:rounded-lg md:p-4 md:h-auto md:w-36 flex flex-col items-center w-16 mr-4 rounded-lg p-2 h-16 bg-[#23354E]">
+                    <div className="lg:text-7xl lg:p-6 md:text-7xl md:p-4 text-3xl font-bold text-[#FFFFFF]">{hours}</div>
+                    <div className="lg:text-2xl md:text-2xl text-sm font-bold text-[#FFFFFF]">Hours</div>
+                </div>
+                <div className="lg:flex lg:flex-col lg:items-center lg:mr-6 lg:rounded-lg lg:p-4 lg:h-auto lg:w-44 md:flex md:flex-col md:items-center md:mr-4 md:rounded-lg md:p-4 md:h-auto md:w-36 flex flex-col items-center w-16 mr-4 rounded-lg p-2 h-16 bg-[#23354E]">
+                    <div className="lg:text-7xl lg:p-6 md:text-7xl md:p-4 text-3xl font-bold text-[#FFFFFF]">{minutes}</div>
+                    <div className="lg:text-2xl md:text-2xl text-sm font-bold text-[#FFFFFF]">Minutes</div>
+                </div>
+                <div className="lg:flex lg:flex-col lg:items-center lg:rounded-lg lg:p-4 lg:h-auto lg:w-44 md:flex md:flex-col md:items-center md:rounded-lg md:p-4 md:h-auto md:w-36 flex flex-col items-center w-16 rounded-lg p-2 h-16 bg-[#23354E]">
+                    <div className="lg:text-7xl lg:p-6 md:text-7xl md:p-4 text-3xl font-bold text-[#FFFFFF]">{seconds}</div>
+                    <div className="lg:text-2xl md:text-2xl text-sm font-bold text-[#FFFFFF]">Seconds</div>
+                </div>
             </div>
-            <div className="flex flex-col items-center mr-6 rounded-lg p-2 bg-gray-200 sm:w-1/8 sm:mt-0" style={{ background: "#23354E" }}>
-                <div className="text-4xl sm:text-12xl font-bold text-7xl p-8" style={{ color: '#FFFF' }}>{hours}</div>
-                <div className="text-2xl font-bold text-2xl" style={{ color: '#FFFF' }}>Hours</div>
-            </div>
-            <div className="flex flex-col items-center mr-6 rounded-lg p-2 bg-gray-200 sm:w-1/8" style={{ background: "#23354E" }}>
-                <div className="text-4xl sm:text-12xl font-bold text-7xl p-8" style={{ color: '#FFFF' }}>{minutes}</div>
-                <div className="text-2xl font-bold text-2xl" style={{ color: '#FFFF' }}>Minutes</div>
-            </div>
-            <div className="flex flex-col items-center mr-6 rounded-lg p-2 bg-gray-200 sm:w-1/8 sm:mt-0" style={{ background: "#23354E" }}>
-                <div className="text-4xl sm:text-12xl font-bold text-7xl p-8" style={{ color: '#FFFF' }}>{seconds}</div>
-                <div className="text-2xl font-bold text-2xl" style={{ color: '#FFFF' }}>Seconds</div>
-            </div>
-        </div>
         </div>
     );
 }
