3cd6a28828d182075a3e15262b52d20d08059212
Ayushi <fierypitsofhello@gmail.com>
2023-04-10 18:25:19 +0530

    navbar size

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,56 +11,52 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="fixed top-0 left-0 right-0 bg-[#23354E] px-5 py-5 flex justify-between items-center z-10">
-            <img src={require('../assets/images/hackman_logo.png')} alt="Logo" className="text-[#E5F2FF] font-medium h-8" />
-          
-            <div className="flex hidden md:block">
-                <a href="#landing" className="text-[#22C3FF] font-small sm:text-2xl text-[18px] sm:mr-8 mr-3">Home</a>
-                <a href="#themes" className="text-[#22C3FF] font-small sm:text-2xl text-[18px] sm:mr-8 mr-3">Themes</a>
-                <a href="#schedule" className="text-[#22C3FF] font-small sm:text-2xl text-[18px] sm:mr-8 mr-3">Schedule</a>
-                <a href="#sponsors" className="text-[#22C3FF] font-small sm:text-2xl text-[18px] sm:mr-8 mr-3">Sponsors</a>
-                <a href="#faq" className="text-[#22C3FF] font-small sm:text-2xl text-[18px] sm:mr-8 mr-3">FAQs</a>
+        <nav className="fixed top-0 left-0 right-0 bg-[#23354E] px-5 py-1 md:py-1 flex justify-between items-center z-10">
+            
+            {/* Logo in navbar */}
+            <img src={require('../assets/images/hackman_logo.png')} alt="Logo" className="h-4 md:h-4 lg:h-5 ml-1" />
+
+            {/* Navbar for large screens */}
+            {/* The navigation elements div */}
+            <div className="flex hidden lg:block">
+
+                <a href="#landing" className="text-[#22C3FF] text-base  text-[18px] sm:mr-8 mr-3">Home</a>
+                <a href="#themes" className="text-[#22C3FF] text-base  text-[18px] sm:mr-8 mr-3">Themes</a>
+                <a href="#schedule" className="text-[#22C3FF] text-base  text-[18px] sm:mr-8 mr-3">Schedule</a>
+                <a href="#sponsors" className="text-[#22C3FF] text-base  text-[18px] sm:mr-8 mr-3">Sponsors</a>
+                <a href="#faq" className="text-[#22C3FF] text-base  text-[18px] sm:mr-8 mr-3">FAQs</a>
+           
             </div>
-            <button className="hidden md:block pt-1 pb-2 right-0 sm:px-8 py-3 rounded-lg sm:mr-8" style={{ backgroundColor: "#D4DFC7" }}>
-                <a href="/Registration" target="_blank" className="text-[#071E22] font-medium sm:text-xl text-lg">Register</a>
+
+            {/* Regiter button */}
+            <button className="hidden lg:block pt-0.5 pb-1 right-0 px-5 py-3 rounded-lg mt-2 mb-2 mr-2" style={{ backgroundColor: "#D4DFC7" }}>
+                <a href="/Registration" target="_blank" className="text-[#071E22] font-medium text-base">Register</a>
             </button>
 
-            <button className="text-[#E5F2FF] font-medium text-2xl focus:outline-none sm:hidden" onClick={() => setIsOpen(!isOpen)}>
-                <img src={require('../assets/images/Menu.png')} alt="Menu" className="h-10 right-0" color='#D4DFC7' />
+
+            {/* Navbar for small and medium screen */}
+            {/* Drop down menu */}
+            {/* Menu button for small and medium screen */}
+            <button className="focus:outline-none lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+                <img src={require('../assets/images/Menu.png')} alt="Menu" className="h-8 md:h-8 right-0" color='#D4DFC7' />
             </button>
-            <div className={`${isOpen ? "block" : "hidden"} sm:hidden absolute top-20 left-0 right-0 bg-[#23354E] text-center rounded-lg pb-4`}>
-                <a href="#" className="block text-[#E5F2FF] font-medium text-2xl mb-1" onClick={() => {
-                    setIsOpen(!isOpen);
-                    jump("landing");
-                }}>Home</a>
 
-                <a href="#" className="block text-[#E5F2FF] font-medium text-2xl mb-1" onClick={() => {
-                    setIsOpen(!isOpen);
-                    jump("themes");
-                }}>Themes</a>
+            {/* Navigation elements inside dropdown menu */}
+            <div className={`${isOpen ? "block" : "hidden"} lg:hidden absolute top-10 left-0 right-0 bg-[#23354E] text-center rounded-lg pb-4`}>
 
-                <a href="#" className="block text-[#E5F2FF] font-medium text-2xl mb-1" onClick={() => {
-                    setIsOpen(!isOpen);
-                    jump("schedule");
-                }}>Schedule</a>
+                <a href="#" className="block text-[#E5F2FF] font-bold text-sm text-2xl mb-1" onClick={() => { setIsOpen(!isOpen); }} >Home</a>
+                <a href="#themes" className="block text-[#E5F2FF] font-bold text-sm mb-1" onClick={() => { setIsOpen(!isOpen); }} >Themes</a>
+                <a href="#schedule" className="block text-[#E5F2FF] font-bold text-sm mb-1" onClick={() => { setIsOpen(!isOpen); }} >Schedule</a>
+                <a href="#sponsors" className="block text-[#E5F2FF] font-bold text-sm mb-1" onClick={() => { setIsOpen(!isOpen); }} >Sponsors</a>
+                <a href="#faq" className="block text-[#E5F2FF] font-bold text-sm mb-2" onClick={() => { setIsOpen(!isOpen); }} >FAQ</a>
 
-                <a href="#" className="block text-[#E5F2FF] font-medium text-2xl mb-1" onClick={() => {
-                    setIsOpen(!isOpen);
-                    jump("sponsors");
-                }}>Sponsors</a>
-
-                <a href="#" className="block text-[#E5F2FF] font-medium text-2xl mb-2" onClick={() => {
-                    setIsOpen(!isOpen);
-                    jump("faq");
-                }}>FAQ</a>
-
-                <button onClick={() => {
-                    setIsOpen(!isOpen);
-                    // jump("registration");
-                }} className="pt-1 pb-2 sm:px-8 px-3 rounded-lg sm:mr-8" style={{ backgroundColor: "#D4DFC7" }}>
-                    <a href="/registration" target="_blank" className="text-[#071E22] font-medium sm:text-xl text-lg">Register</a>
+                {/* Register button inside dropdown menu*/}
+                <button onClick={() => { setIsOpen(!isOpen); }} className="pb-1 px-2 rounded-lg" style={{ backgroundColor: "#D4DFC7" }}>
+                    <a href="/registration" target="_blank" className="text-[#071E22] font-medium text-sm">Register</a>
                 </button>
+
             </div>
+
         </nav>
     );
 }
