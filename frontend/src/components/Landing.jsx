d1646f9b6d427e929ac67155e9367b2df2a168b9
Aditi <aditipandeya1608@gmail.com>
2023-04-24 09:59:51 +0530

    aligned with the margin

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -6,7 +6,7 @@ function Landing() {
       <section className="bg-[#27273F]">
         <div className="items-center justify-center">
           <div className="items-center justify-center relative">
-            <img className="w-full" alt="Poster1" src={require('../assets/images/eva_main.png')} />
+            <img className="w-full h-100vh" alt="Poster1" src={require('../assets/images/eva_main.png')} />
           </div>
         </div>
       </section>
