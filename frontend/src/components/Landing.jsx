e6b5d91439d8479f31002432a46ecb11152f9760
Astraxx04 <gagan200254@gmail.com>
2023-04-28 16:04:10 +0530

    Image and docs

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -6,7 +6,7 @@ function Landing() {
             <section className="bg-[#27273F] h-screen">
                 <div className="items-center justify-center h-screen">
                     <div className="items-center justify-center relative h-screen">
-                        <img className="w-full h-full object-cover object-top" alt="Poster1" src={require('../assets/images/eva_main.png')} />
+                        <img className="w-full h-full object-cover object-top" alt="Poster1" src={require('../assets/images/eva_main.jpg')} />
                     </div>
                 </div>
             </section>
