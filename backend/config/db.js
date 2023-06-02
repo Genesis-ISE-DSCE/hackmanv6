6c530a46ae5a1b322c7a7d547019c238b6425a84
Janesh7 <janeshwalia273@gmail.com>
2023-06-02 19:39:45 +0530

    Final touches

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const connectWithDb = (process.env.DB_URL)=>{
-    mongoose.connect(DB_URL,{
+const connectWithDb = ()=>{
+    mongoose.connect(process.env.DB_URL,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(()=>(console.log(`Db connected sucessfully`)))
