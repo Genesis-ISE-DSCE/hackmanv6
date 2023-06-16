6dc35602147c879c5c079c5f515c9dc31fe30b61
heisdinesh <dinesh35382@gmail.com>
2023-06-16 23:19:16 +0530

    mass mail added

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import MyTableComponent from '../components/Admin/MyTableComponent'
 
 const Admin = () => {
+  const navigate = useNavigate()
   return (
-    <div><MyTableComponent /></div>
+    <div>
+      <div className='flex items-center justify-center py-4 '>
+        <p className='text-4xl'>Admin Pannel</p>
+        <button
+                className="position right-8  absolute lg:text-xl shadow bg-[#22C3FF] hover:bg-[#D4DFC7] hover:text-black focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded text-sm"
+                 onClick={()=>{navigate("/mass-mail")}}>Mail</button>
+      </div>
+      <MyTableComponent />
+    </div>
   )
 }
 
